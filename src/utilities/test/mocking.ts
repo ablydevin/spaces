c784907d61de3b0e1100ffddd41619136ae986b7
Lawrence Forooghian <lawrence@forooghian.com>
2023-10-18 13:59:13 -0300

    Check sender of ENTER presence message in space.enter()
    It shouldn’t return as a result of somebody else entering the space.
    
    I’m not sure whether this is the best way of predicating whether or not
    to return — perhaps checking for some unique ID on the received ENTER
    presence message would be better. But it seems like an OK low-thought
    solution at least for the time being.

--- src/utilities/test/mocking.ts
+++ src/utilities/test/mocking.ts
@@ -3,14 +3,18 @@ import { Types } from 'ably/promises';
 import { createPresenceMessage } from './fakes.js';
 
 /**
- * Mocks `presence`’s `subscribe` function (the overload which accepts one or more actions and a listener) to immediately call the listener with a PresenceMessage of (arbitarily-chosen) action `ENTER`.
+ * Mocks `presence`’s `subscribe` function (the overload which accepts one or more actions and a listener) to immediately call the listener with a PresenceMessage of:
+ *
+ * - action `ENTER` (arbitrarily-chosen)
+ * - connectionId '1' (to match that used by the ably-js auto-mock)
+ * - clientId 'MOCK_CLIENT_ID' (to match that used by the ably-js auto-mock)
  */
 export function mockSubscribeForSpaceEnter(presence: Types.RealtimePresencePromise) {
   const subscribeImplementation: (typeof presence)['subscribe'] = async (
     _,
     listener?: (presenceMessage: Types.PresenceMessage) => void,
   ) => {
-    listener!(createPresenceMessage('enter'));
+    listener!(createPresenceMessage('enter', { clientId: 'MOCK_CLIENT_ID', connectionId: '1' }));
   };
   vi.spyOn(presence, 'subscribe').mockImplementation(subscribeImplementation);
 }
